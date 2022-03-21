db747abd58d584a87390b837013db88736adc907
AlbertBaby <albertbaby266192@gmail.com>
2022-03-21 22:40:08 +0530

    code clean

--- Selenium/seleniumExample.js
+++ Selenium/seleniumExample.js
@@ -1,6 +1,7 @@
 const{Builder, By, Key, until} = require('selenium-webdriver');
 require('chromedriver');
 
+
 async function example() {
     let driver = await new Builder().forBrowser('chrome').build();
     await driver.get('http://www.google.com');
